c303026f75ecb5beee26db29355614a80bc9cc08
BaronZ <realbo89@outlook.com>
2013-05-11 15:48:26 +0800

    move UDP demos to folder UDP-demos

---
+++ UDP-demos/UDPsend.java
@@ -0,0 +1,37 @@
+package com.baron.socket.demo;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
+
+/*
+ * 1.建立UDP socket服务(通过DatagramSocket对象)
+ * 2.提供数据，并交数据封装到数据包中(通过DatagramPacket对象)
+ * 3.通过socket服务的发送功能，将数据包发送出去ds.send(dp);
+ * 4.关闭资源
+ * */
+public class UDPsend {
+	
+	public static void main(String[] args) throws IOException {
+		//1 创建UDP服务，通过DatagramSocket对象
+		DatagramSocket ds = new DatagramSocket();
+		//2. 确定数据，并封装成数据包。
+		//DatagramPacket(byte[] buf, int len,InetAddress add,int port);
+		//buf->data, len->data length, add->the target app's IP, port->the target app's port
+		byte[] buf = "UDP MSG".getBytes();
+		DatagramPacket dp = 
+				new DatagramPacket(buf, buf.length, InetAddress.getByName("192.168.1.254"),10000);
+		//3.通过socket服务，将已有的数据包发送出去
+		ds.send(dp);
+		//4.关闭资源
+		ds.close();
+	}
+
+}
+
+/*
+ * 
+ * 
+ * */
